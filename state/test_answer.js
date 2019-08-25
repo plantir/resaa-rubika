3c429061c89f3ba475845060793c4c3d4928f47f
arminkheirkhahan <arminkheirkhahan@gmail.com>
2019-08-25 16:41:54 +0430

    add doctor

--- state/test_answer.js
+++ state/test_answer.js
@@ -43,11 +43,7 @@ bot.onText(_enum.regex_state.test_answer, async msg => {
       data
     });
   } else if (test_answer.status === 'needMoney') {
-    message = `اعتبار فعلی شما ${
-      test_answer.user_charge
-    } تومان میباشد و در خواست شما نیاز به ${
-      test_answer.request_price
-    } تومان شارژ دارد `;
+    message = `اعتبار فعلی شما ${test_answer.user_charge} تومان میباشد و در خواست شما نیاز به ${test_answer.request_price} تومان شارژ دارد `;
     rows.push({
       buttons: [
         {
@@ -89,6 +85,7 @@ bot.onText(_enum.regex_state.test_answer, async msg => {
       data
     });
   } else if (test_answer.status === 'needTalk') {
+    let doctor = await user.last_visit_doctor;
     user.state = _enum.state.doctor_detail;
     message = `برای ارسال جواب آزمایش نیاز به هماهنگی قبلی با پزشک هست.\nشما در ۲۴ ساعت اخیر با این پزشک مکالمه ای نداشته اید لطفا ابتدا با پزشک خود مکالمه کنید سپس جواب آزمایش را ارسال نمایید`;
     rows.push({
@@ -130,9 +127,7 @@ bot.onText(_enum.regex_state.test_answer, async msg => {
       data
     });
   }
-  message = `هزینه جواب آزمایش ${
-    test_answer.request_price
-  } تومان می باشد و در صورت ارسال فایل از شارژ رسا شما کم میشود\nدر صورت تایید عکس آزمایش خود را بفرستید`;
+  message = `هزینه جواب آزمایش ${test_answer.request_price} تومان می باشد و در صورت ارسال فایل از شارژ رسا شما کم میشود\nدر صورت تایید عکس آزمایش خود را بفرستید`;
   user.state = _enum.state.test_answer;
   rows.push({
     buttons: [
@@ -203,9 +198,7 @@ bot.on('file', async msg => {
     return;
   }
   let files = await user.add_file(msg.file_inline.file_url);
-  let message = `شما تا کنون ${
-    files.length
-  } فایل پیوست کرده اید اگر فایل دیگری هم دارید ارسال کنید در غیر اینصورت بر روی دکمه اتمام کلیک کنید`;
+  let message = `شما تا کنون ${files.length} فایل پیوست کرده اید اگر فایل دیگری هم دارید ارسال کنید در غیر اینصورت بر روی دکمه اتمام کلیک کنید`;
   let data = {
     bot_keypad: {
       rows: [
@@ -267,9 +260,7 @@ bot.onText(_enum.regex_state.finish_file_upload, async msg => {
     let { tracking_code, count } = await user.send_testAnswer(
       test_answer.chat_id
     );
-    message = `جواب آزمایش شما با موفقیت برای دکتر ${doctor.firstName} ${
-      doctor.lastName
-    } ارسال شد\n کد پیگیری جواب آزمایش شما ${tracking_code}`;
+    message = `جواب آزمایش شما با موفقیت برای دکتر ${doctor.firstName} ${doctor.lastName} ارسال شد\n کد پیگیری جواب آزمایش شما ${tracking_code}`;
     data = {
       bot_keypad: {
         rows: [
